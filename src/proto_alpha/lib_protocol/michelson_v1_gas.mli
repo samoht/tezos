tezos/tezos-mirror

Proto/Michelson: Add comparable `pvss_key` type

src/proto_alpha/lib_protocol/michelson_v1_gas.mli
@@ -255,6 +255,8 @@ module Cost_of : sig
 
     val baker_hash : Gas.cost
 
+    val pvss_key : Gas.cost
+
     val signature : Gas.cost
 
     val bls12_381_g1 : Gas.cost
@@ -332,6 +334,8 @@ module Cost_of : sig
     val key_hash : Gas.cost
 
     val baker_hash : Gas.cost
+
+    val pvss_key : Gas.cost
 
     val signature : Gas.cost
 

samoht/tezos

src/proto_alpha/lib_protocol/michelson_v1_gas.mli
@@ -318,6 +318,10 @@ module Cost_of : sig
 
     val baker_hash_readable : Gas.cost
 
+    val pvss_key_optimized : Gas.cost
+
+    val pvss_key_readable : Gas.cost
+
     val signature_optimized : Gas.cost
 
     val signature_readable : Gas.cost
@@ -377,6 +381,10 @@ module Cost_of : sig
     val baker_hash_optimized : Gas.cost
 
     val baker_hash_readable : Gas.cost
+
+    val pvss_key_optimized : Gas.cost
+
+    val pvss_key_readable : Gas.cost
 
     val signature_optimized : Gas.cost
 