917b889b95552b1018a78ff1a3c7831b8561a2f6
Thomas Gazagnaire <thomas@gazagnaire.org>
2021-01-08 10:10:28 +0100

    Env: deduplicate module type definitions
    Add an Environment_context_intf module to hold these signatures.

--- src/lib_protocol_environment/environment_context_intf.ml
+++ src/lib_protocol_environment/environment_context_intf.ml
@@ -23,31 +23,32 @@
 (*                                                                           *)
 (*****************************************************************************)
 
-(** @inline *)
-module type CONTEXT = Environment_context_intf.S
+module type S = sig
+  type t
 
-module Context : sig
-  type 'ctxt ops = (module CONTEXT with type t = 'ctxt)
+  type key = string list
 
-  type _ kind = ..
+  type value = Bytes.t
 
-  type t = Context : {kind : 'a kind; ctxt : 'a; ops : 'a ops} -> t
+  val mem : t -> key -> bool Lwt.t
 
-  include CONTEXT with type t := t
+  val dir_mem : t -> key -> bool Lwt.t
+
+  val get : t -> key -> value option Lwt.t
+
+  val set : t -> key -> value -> t Lwt.t
+
+  val copy : t -> from:key -> to_:key -> t option Lwt.t
+
+  val remove_rec : t -> key -> t Lwt.t
+
+  type key_or_dir = [`Key of key | `Dir of key]
+
+  val fold :
+    t -> key -> init:'a -> f:(key_or_dir -> 'a -> 'a Lwt.t) -> 'a Lwt.t
+
+  val set_protocol : t -> Protocol_hash.t -> t Lwt.t
+
+  val fork_test_chain :
+    t -> protocol:Protocol_hash.t -> expiration:Time.Protocol.t -> t Lwt.t
 end
-
-type validation_result = {
-  context : Context.t;
-  fitness : Fitness.t;
-  message : string option;
-  max_operations_ttl : int;
-  last_allowed_fork_level : Int32.t;
-}
-
-type quota = {max_size : int; max_op : int option}
-
-type rpc_context = {
-  block_hash : Block_hash.t;
-  block_header : Block_header.shell_header;
-  context : Context.t;
-}
